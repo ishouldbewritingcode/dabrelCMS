1c5f00507060db2a62e7189bd10243d9604fdf86
Bryan <ishouldbewritingcode@gmail.com>
2024-03-18 23:02:43 -0500

    Fixing the nav.

--- wwwroot/global/scripts/global.js
+++ wwwroot/global/scripts/global.js
@@ -1,21 +1,26 @@
-﻿var nav = document.getElementById('nav');
-var navitems = nav.querySelectorAll('a');
+﻿let nav = document.getElementById('nav');
+let navitems = nav.querySelectorAll('a');
 Array.from(navitems).forEach(function (item) {
 	item.addEventListener('click', navClicked, false);
 });
 
+
 function navClicked(event) {
-	// turn off all paths
-	var paths = nav.querySelectorAll('li.path');
+	// turn off all paths / opens
+	let paths = nav.querySelectorAll('.path');
 	Array.from(paths).forEach(function (p) {
 		p.classList.remove('path');
+	});
+	let opens = nav.querySelectorAll('.open');
+	Array.from(opens).forEach(function (p) {
+		p.classList.remove('open');
 	});
 	// add the correct path
 	this.parentNode.classList.add('path');
 	document.getElementById('nav-toggle').checked = false; //close mobile nav
 }
 
-var subs = nav.getElementsByClassName('sub');
+let subs = nav.getElementsByClassName('sub');
 Array.from(subs).forEach(function (sub) {
 	sub.addEventListener('click', openChildNav, false);
 });
@@ -27,6 +32,10 @@ function openChildNav(event) {
 		this.parentNode.classList.remove('open');
 	}
 	else {
+		let paths = nav.querySelectorAll('li.path');
+		Array.from(paths).forEach(function (p) {
+			p.classList.remove('path');
+		});
 		this.classList.add('open');
 		this.parentNode.classList.add('open');
 	}
